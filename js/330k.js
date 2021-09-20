3f2c06fbca1274a43e1477fe4f259e98489af876
330k <kei.misawa@gmail.com>
2021-09-20 18:51:50 +0900

    rebuilding site 2021年  9月 20日 月曜日 18:51:42 JST

--- js/330k.js
+++ js/330k.js
@@ -1,2 +1,1 @@
-document.querySelectorAll("a[href^='https://']").forEach((e) => e.setAttribute("target", "_blank"));
-document.querySelectorAll("a[href^='http://']").forEach((e) => e.setAttribute("target", "_blank"));
+document.querySelectorAll("a[href^='http://'],a[href^='https:']:not(a[href^='https://www.330k.info/'])").forEach((e) => e.setAttribute("target", "_blank"));
